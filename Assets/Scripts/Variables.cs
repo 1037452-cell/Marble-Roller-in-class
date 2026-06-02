8fa3eb605c7696f80f26bb91bdda24ef13f0b8e7
Braden_port <1037452@student.sae.edu.au>
2026-06-02 20:43:04 +1000

    Completed Variables

---
+++ Assets/Scripts/Variables.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+
+    public string myName = "Braden";
+    public int myAge = 27;
+    public float myFloat = 27.5f;
+    public double myDouble = 27.55;
+    public bool myBool = true;
+    public char myLetter = 'B';
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
