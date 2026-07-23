063825fd21beb6a7cc437e4665ac4f65766b35b2
1037452-cell <1037452@student.sae.edu.au>
2026-07-23 12:01:40 +1000

    Spawner and for loops

--- Assets/Scripts/ForCheck.cs
+++ Assets/Scripts/ForCheck.cs
@@ -1,21 +1,21 @@
 using UnityEngine;
 
-public class Trigger : MonoBehaviour
+public class ForCheck : MonoBehaviour
 {
+
+    public int number = 10;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        for (int i = 0; i < number; i++)
+        {
+            Debug.Log("Yep. Number = " + i);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-       
-    }
-    public void OnTriggerEnter(Collider other)
-    {
-        Debug.Log("Yes");
+        
     }
 }
-
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+
+    public int number = 10;
+    public GameObject cap;
+    public Transform myTransform;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        for (int i = 0; i < number; i++)
+        {
+            Debug.Log("Yep. Number = " + i);
+            Instantiate(cap, myTransform);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
